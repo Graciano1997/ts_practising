d292cdf8f6057e67bfae453928569875eadecffc
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-08-04 08:45:33 -0400

    add the basic three primitive types

---
+++ build/primitive.js
@@ -0,0 +1,8 @@
+"use strict";
+let nome;
+let idade;
+let adulto;
+nome = "Graciano Manuel Henrique";
+idade = 20;
+adulto = true;
+console.log(`Nome:${nome} Idade: ${idade}, Adulto: ${adulto}`);
---
+++ src/primitive.ts
@@ -0,0 +1,9 @@
+let nome: string;
+let idade: number;
+let adulto: boolean;
+
+nome="Graciano Manuel Henrique";
+idade=20;
+adulto=true;
+
+console.log(`Nome:${nome} Idade: ${idade}, Adulto: ${adulto}`);
